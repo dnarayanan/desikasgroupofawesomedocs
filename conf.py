8cda4ff848b45116536c5e3c830218306ec40041
Ethan Savitch <esavitch@c30a-s23.ufhpc>
2023-05-24 11:00:55 -0400

    changed the doc header from powderday to Desikas Docs

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Powderday'
+project = u'Desikas Docs'
 copyright = u'2015, Desika Narayanan'
 
 # The version info for the project you're documenting, acts as replacement for
